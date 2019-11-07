11c66e24c9bc144a3520f28ec825b34092e1185a
SudoWeezy <56608644+SudoWeezy@users.noreply.github.com>
2019-11-07 15:21:42 +0100

    Update gapTickerWebsocket.py

--- Live/gapTickerWebsocket.py
+++ Live/gapTickerWebsocket.py
@@ -40,23 +40,11 @@ class TradeSocket:
         self.pa.set_command("cancelAllOrders", "currencyPair", self.pair)
         self.pa.call_private_api()
         self.pa.set_command(bs, "currencyPair", self.pair, "rate", rate,
-                            "amount", amount, "fillOrKill ", 1)
+                            "amount", amount, "postOnly ", 1)
         trade_result = self.pa.call_private_api()
-        if trade_result['resultingTrades']:
-            self.on_trade = not self.on_trade            
-            rate = float(trade_result['resultingTrades'][0]['rate'])
-            print("Trade has been performed at rate: %f!" % rate)
-            self.asset1, self.asset2 = get_amount(self.pair)
-            self.init_atr()
-            self.sell = rate - self.atr
-            self.buy = rate + self.atr
-            new_info = (self.atr, self.buy, self.sell)
-            print("ATR: %f, BUY_Trig: %f, SELL_Trig: %f" % new_info)
 
     def on_message(self, message):
         json_msg = json.loads(message)
-        if time.time() > (self.p_time + self.period):
-            self.init_atr()
         if len(json_msg) <= 2:
             return
         for i in json_msg[2]:
@@ -66,16 +54,10 @@ class TradeSocket:
                 continue
             rate = float(i[2])
             if i[1] == 1 and rate > self.buy:
-                self.buy += self.atr
-                self.sell = self.buy - self.atr
-                print("Update| SELL %f | BUY: %f" % (self.sell, self.buy))
                 if not self.on_trade:
                     print("B:[Rate: " + i[2] + " Amount: " + i[3] + "]")
                     self.make_trade(rate, "buy")
             elif i[1] == 0 and rate < self.sell:
-                self.sell -= self.atr
-                self.buy = self.sell + self.atr
-                print("Update| BUY: %f| SELL: %f" % self.buy, self.sell)
                 if self.on_trade:
                     print("A:[Rate: " + i[2] + " Amount: " + i[3] + "]")
                     self.make_trade(rate, "sell")
@@ -89,25 +71,6 @@ class TradeSocket:
         print("### closed ###")
         print("Close time: %s" % time.ctime(time.time()))
     
-    def init_atr(self):
-        p_time, self.atr = get_trigger(self.pair, self.period, self.step)
-        if self.p_time != p_time:
-            self.pa.set_command("returnTradeHistory",
-                                "currencyPair", self.pair,
-                                "start", 1547164800)
-            trade_history = self.pa.call_private_api()
-            self.on_trade = trade_history[0]["type"] == "buy"
-            self.sell = float(trade_history[0]["rate"]) - self.atr
-            self.buy = float(trade_history[0]["rate"]) + self.atr
-            self.p_time = p_time
-            self.print_info()
-
-    def print_info(self):
-        print("Atr Datetime: %s" % time.ctime(self.p_time))
-        print("Buy Trigger: %f Sell Trigger: %f" % (self.buy, self.sell))
-        print("Current Datetime: %s" % time.ctime(time.time()))
-        print("Pair %s : %f, %f" % (self.pair, self.asset1, self.asset2))
-        print("On trade: %r" % self.on_trade)
 
     def on_open(self):
         print("ON OPEN")
