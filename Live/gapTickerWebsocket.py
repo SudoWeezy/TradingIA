ee65cc7a3fbf1ef5422961887ccc87ae7a4e0c38
SudoWeezy <56608644+SudoWeezy@users.noreply.github.com>
2019-11-08 19:04:05 +0100

    Update gapTickerWebsocket.py

--- Live/gapTickerWebsocket.py
+++ Live/gapTickerWebsocket.py
@@ -78,11 +78,11 @@ class TradeSocket:
                         self.ask = i[1]['orderBook'][1]
                     min_bid = min(map(float, self.bid.keys()))
                     max_ask = max(map(float, self.ask.keys()))
-                if self.rate_sell != min_bid * 1.0005:
+                if self.rate_sell < min_bid:
                     self.rate_sell = min_bid * 1.0005
                     self.trade("sell", self.rate_sell, self.asset2)
 
-                if self.rate_buy != max_ask * 0.9995:
+                if self.rate_buy > max_ask:
                     self.rate_buy = max_ask * 0.9995
                     self.trade("buy", self.rate_buy, self.asset1/self.rate_buy)
 
