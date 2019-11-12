93d8ece6b4f532ce336c9f35643692331fe111b3
SudoWeezy <56608644+SudoWeezy@users.noreply.github.com>
2019-11-12 17:28:21 +0100

    Update main.py

--- Live/main.py
+++ Live/main.py
@@ -5,13 +5,14 @@ if __name__ == "__main__":
     pair = "USDT_BTC"
     period_calc = 14400
     step_calc = 15
-    while(1):
+    do_on = True
+    while do_on:
         try :
             socketCall = TradeSocket(pair, period_calc, step_calc)
 
             socketCall.ws.run_forever()
         except KeyError:
             print("Clean Exit")
-            break
+            do_on = False
         except Exception as e:
             print(e)
