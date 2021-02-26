5eea245ede0423a10098cc13dae7cad665187e81
sudoweezy <sudoweezy@gmail.com>
2021-02-26 23:58:57 +0100

    add crypto.com api

---
+++ LiveCryptocom/api.py
@@ -0,0 +1,134 @@
+import requests
+import hmac
+import hashlib
+import time
+import json
+
+link_public = "https://api.crypto.com/v2/"
+link_api = "https://api.crypto.com/v2/"
+link_websocket = "wss://stream.crypto.com/v2/market"
+list_public = ["public/auth",
+               "public/get-instruments",
+               "public/get-book",
+               "public/get-candlestick",
+               "public/get-ticker",
+               "public/get-trades",
+               "public/respond-heartbeat"
+               ]
+list_private = ["private/set-cancel-on-disconnect",
+                "private/get-cancel-on-disconnect",
+                "private/create-withdrawal",
+                "private/get-withdrawal-history",
+                "private/get-deposit-history",
+                "private/get-account-summary",
+                "private/create-order",
+                "private/cancel-order",
+                "private/cancel-all-orders",
+                "private/get-order-history",
+                "private/cancelOrder",
+                "private/get-open-orders",
+                "private/get-order-detail",
+                "private/get-trades"
+                ]
+
+list_subscription = ["user.order.{instrument_name}",
+                     "user.trade.{instrument_name}",
+                     "user.balance",
+                     "book.{instrument_name}.{depth}",
+                     "ticker.{instrument_name}",
+                     "trade.{instrument_name}",
+                     "candlestick.{interval}.{instrument_name}"
+                     ]
+
+
+class ConnectApi:
+    def __init__(self, exchange="cryptocom", link=link_public):
+        """
+        Parameters
+        ----------
+        link : String
+            Warper for Crypto.com Api
+            For information https://exchange-docs.crypto.com/spot/index.html
+        """
+        self.link = link
+        self.headers = {'Content-Type': 'application/json'}
+        self.payload = {}
+        with open("home/Env/.zshenv") as f:
+            dict_info = json.load(f)
+        self.api_key = dict_info[exchange]["key"]
+        self.api_sign = bytes(dict_info[exchange]["sign"], "utf8")
+        self.response = ''
+
+    def call_public_api(self):
+        """
+        Returns
+        -------
+        type : json
+            Return response from the public api in json format
+            see set_command to create your call
+        """
+        r = requests.get(self.link + self.payload['method'],
+                         params=self.payload['params'])
+        if r.status_code == 200:
+            self.response = r.json()
+            return self.response
+        else:
+            error_msg = "Error %s during get request" % r.status_code
+            print(error_msg)
+
+    def set_payload(self, _id):
+        self.payload["api_key"] = self.api_key
+        self.payload["id"] = _id
+        self.payload["nonce"] = int(time.time() * 1000)
+        req = self.payload
+        params = req["params"]
+        load = req['method'] + str(req['id']) + req['api_key']
+        load = load + ''.join('{}{}'.format(*p) for p in sorted(params.items()))
+        msg = bytes(load + str(req['nonce']), 'utf-8')
+
+        self.payload['sig'] = hmac.new(self.api_sign, msg=msg,
+                                       digestmod=hashlib.sha256).hexdigest()
+
+    def call_private_api(self, _id):
+        """
+        Returns
+        -------
+        type : json
+            Return response from the Trading api in json format
+            see set_command to create your call
+        """
+        self.set_payload(_id)
+        request = requests.post(self.link + self.payload['method'],
+                                json=self.payload,
+                                headers=self.headers)
+        self.response = request.json()
+        return self.response
+
+    def set_command(self, command, **kwargs):
+        """
+        Parameters
+        ----------
+        command : String
+            You can see all the command on https://poloniex.com/support/api/
+
+        kwargs : multiple dict
+            You can set the option like that:
+            set_command("public/get-book", instrument_name="BTC_USDT",
+                        page_size=2)
+        """
+        if (command in list_public) or (command in list_private):
+            self.payload = {'method':  command, 'params': kwargs}
+        else:
+            error_msg = "Wrong link or command"
+            print(error_msg)
+
+
+if __name__ == "__main__":
+    CA = ConnectApi()
+    CA.set_command("private/get-open-orders", instrument_name="BTC_USDC",
+                   page_size=2)
+    print(CA.call_private_api(11))
+    CA.set_command("public/get-book", instrument_name="BTC_USDT")
+    CA.call_public_api()
+    CA.set_command("public/get-instruments")
+    CA.call_public_api()
---
+++ LivePoloniex/gapTickerWebsocket.py
@@ -0,0 +1,113 @@
+import json
+from threading import Thread
+from livetrade import  get_amount
+import websocket
+from api import ConnectApi
+import time
+
+
+class TradeSocket:
+    def __init__(self, pair, period, step):
+        websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp("wss://stream.crypto.com/v2/user",
+                                         on_message = self.on_message,
+                                         on_error = self.on_error,
+                                         on_close = self.on_close)
+        self.pair = pair
+        self.period = period
+        self.step = step
+        self.bid = {}
+        self.ask = {}
+        self.buy = 0
+        self.p_time = 0
+        self.rate_sell = 0
+        self.rate_buy = float('Inf')
+        self.asset1, self.asset2 = get_amount(self.pair)
+        self.ws.on_open = self.on_open
+        self.pa = ConnectApi("https://poloniex.com/tradingApi")
+        self.on_trade = False
+        self.current_order_buy = ''
+        self.current_order_sell = ''
+
+    def trade(self, bs, rate, amount):
+        self.asset1, self.asset2 = get_amount(self.pair)
+        self.pa.set_command("returnOpenOrders", "currencyPair", self.pair)
+        response = self.pa.call_private_api()
+        self.current_order_buy = []
+        self.current_order_sell = []
+        if bs == "buy":
+            for i in response:
+                if i["type"] == "buy":
+                    self.current_order_buy = i["orderNumber"]
+                    self.rate_buy = float(i["rate"])
+                    if self.current_order_buy:
+                        self.pa.set_command("moveOrder",
+                                            "orderNumber", self.current_order_buy,
+                                            "rate", rate)
+            if not response:
+                self.pa.set_command(bs, "currencyPair", self.pair, "rate", rate,
+                                    "amount", amount, "postOnly ", 1)
+            self.pa.call_private_api()
+
+        elif bs == "sell":
+            for i in response:
+                if i["type"] == "sell":
+                    self.current_order_sell = i["orderNumber"]
+                    self.rate_sell = float(i["rate"])
+                    if self.current_order_sell:
+                        self.pa.set_command("moveOrder",
+                                            "orderNumber", self.current_order_sell,
+                                            "rate", rate)
+            if not response:
+                self.pa.set_command(bs, "currencyPair", self.pair, "rate",
+                                    rate, "amount", amount, "postOnly ", 1)
+            self.pa.call_private_api()
+
+    def on_message(self, message):
+        json_msg = json.loads(message)
+        if len(json_msg) > 2:
+            for i in json_msg[2]:
+                if i[0] == "o":
+                    if i[1] == 1:
+                        self.ask[i[2]] = i[3]
+                        if float(i[3]) == 0:
+                            self.ask.pop(i[2])
+                    elif i[1] == 0:
+                        self.bid[i[2]] = i[3]
+                        if float(i[3]) == 0:
+                            self.bid.pop(i[2])
+                elif i[0] == "i":
+                    self.bid = i[1]['orderBook'][0]
+                    self.ask = i[1]['orderBook'][1]
+                min_bid = min(map(float, self.bid.keys()))
+                max_ask = max(map(float, self.ask.keys()))
+
+            min_bid_plus = min_bid * 1.0001
+            if self.rate_sell <= min_bid or self.rate_sell > min_bid_plus:
+                self.rate_sell = min_bid_plus
+                self.trade("sell", self.rate_sell, self.asset2/2)
+
+            max_ask_plus = max_ask * 0.9999
+            if self.rate_buy >= max_ask or self.rate_buy < max_ask_plus:
+                self.rate_buy = max_ask_plus
+                self.trade("buy", self.rate_buy, self.asset1/2/self.rate_buy)
+
+    @staticmethod
+    def on_error(self, error):
+        print(error)
+        self.on_close()
+
+    def on_close(self):
+        self.pa.set_command("cancelAllOrders", "currencyPair", self.pair)
+        self.pa.call_private_api()
+        print("### closed ###")
+        print("Close time: %s" % time.ctime(time.time()))
+
+    def on_open(self):
+        print("ON OPEN")
+ 
+        def run():
+            self.ws.send(json.dumps({'command': 'subscribe',
+                                     'channel': self.pair}))
+
+        Thread(target=run).start()
