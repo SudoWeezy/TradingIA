6cb1ddceff9da5009973180e09d21fbd0a833d6b
sudoweezy <sudoweezy@gmail.com>
2021-03-02 15:28:59 +0100

    reference en parametre shell

--- dca_tool.sh
+++ dca_tool.sh
@@ -5,7 +5,7 @@ then
 	L_DATE=$(date +'%Y_%m_%d')
 	L_LOG_FILE="log/LOG_DCA_${L_DATE}"
 	source "${L_PYTHON_ENV}"
-	python "${L_PYTHON_SCRIPT}" "reference=USDT" "withdraw=NO" > $L_LOG_FILE
+	python "${L_PYTHON_SCRIPT}" "$1" "$2" > $L_LOG_FILE
 	L_RESULT=$(cat $L_LOG_FILE| grep ERROR | wc -l)
 	if [ "${L_RESULT}" -eq 0 ]
 	then
