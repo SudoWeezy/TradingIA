fd819eac966e3cf5156425e34749057806c60fa8
sudoweezy <sudoweezy@gmail.com>
2021-03-02 16:35:33 +0100

    update python requirement

--- dca_tool.sh
+++ dca_tool.sh
@@ -2,11 +2,11 @@ L_REPERTOIRE="${PWD}/$(dirname $0)"
 L_PYTHON_ENV="${L_REPERTOIRE}/env/bin/activate"
 L_PYTHON_SCRIPT="${L_REPERTOIRE}/LiveCryptocom/dca.py"
 
-
+echo "Repertoire $L_REPERTOIRE" 
 if [ -f "${L_PYTHON_ENV}" ]
 then
 	L_DATE=$(date +'%Y_%m_%d')
-	L_LOG_FILE="${L_REPERTOIRE}/LiveCryptocom/log/LOG_DCA_${L_DATE}"
+	L_LOG_FILE="${L_REPERTOIRE}/log/LOG_DCA_${L_DATE}"
 	source "${L_PYTHON_ENV}"
 	python "${L_PYTHON_SCRIPT}" "$1" "$2" > $L_LOG_FILE
 	L_RESULT=$(cat $L_LOG_FILE| grep ERROR | wc -l)
